c06c20e068352da1643537e8cb7e96ddec0aa4ff
heuristiclife <friud38@naver.com>
2021-07-01 13:12:11 +0900

    0701_Zeda_1

---
+++ Source/MyProjectxx/MyProjectxx.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyProjectxx.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProjectxx, "MyProjectxx" );
